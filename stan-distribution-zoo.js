ccddb719df80b3455fd6c3da3308a24ab3440a0e
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-11-29 13:53:41 +0100

    fix normal_lpdf

--- stan-distribution-zoo.js
+++ stan-distribution-zoo.js
@@ -1,3 +1,32 @@
+function add_figure() {
+
+    update_data()
+
+    const margin = { top: 30, right: 30, bottom: 30, left: 30 },
+        width = $("#figure").width() - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    svg = d3.select("#figure")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    x = d3.scaleLinear().range([0, width]);
+    xAxis = d3.axisBottom().scale(x);
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .attr("class", "myXaxis")
+
+    y = d3.scaleLinear().range([height, 0]);
+    yAxis = d3.axisLeft().scale(y);
+    svg.append("g")
+        .attr("class", "myYaxis")
+
+    update()
+}
+
 function setBubble(range, bubble) {
     const val = range.val();
     const min = range.attr("min");
@@ -22,10 +51,16 @@ function setup_page(settings) {
                 + '" max="' + settings.sliders[i].max + '" step="' + settings.sliders[i].step
                 + '" id="' + settings.sliders[i].name + '" name="' + settings.sliders[i].name + '" >');
             var out = $('<output class="bubble"></output>');
-            inp.on("input", function() {
-                
+            inp.on("input", function () {
+
                 setBubble($(this), $(this).parent().children('.bubble'))
             });
+            inp.on("change", function () {
+                $(".range").each(function() {
+                    params[$(this).attr("name")] = $(this).val();
+                });
+                update()
+            })
             setBubble(inp, out);
             var slider = $('<div class="range-wrap"></div>');
             body.append(slider.append(label).append(inp).append(out));
@@ -34,31 +69,44 @@ function setup_page(settings) {
             var exp_check = $(
                 '<div class="range-wrap">' +
                 '<label class="container">Exponantiate<input type="checkbox" id="exp-check" ><span class="checkmark" id="exp"></span></label></div>');
+            exp_check.on("change", function () {
+                    update()
+            })
             body.append(exp_check);
         }
-        
         add_figure()
-        // d3.select("#y_range_slider").on("change", function(d){
-        //     y_range = this.value
-        //     update_data()
-        //     update(data1)
-        // })
-        // d3.select("#mu_slider").on("change", function(d){
-        //     mu = this.value
-        //     update_data()
-        //     update(data1)
-        // })
-        // d3.select("#sigma_slider").on("change", function(d){
-        //     sigma = this.value
-        //     update_data()
-        //     update(data1)
-        // })
-        // d3.select("#exp-check").on("change", function(d){
-        //     exponantiate = $("#exp-check")[0].checked
-        //     update_data()
-        //     update(data1)
-        // })
     });
 
 
 }
+
+function update() {
+    update_data()
+
+    x.domain([d3.min(data, function (d) { return d.ser1 }), d3.max(data, function (d) { return d.ser1 })]);
+    svg.selectAll(".myXaxis").transition()
+        .duration(3000)
+        .call(xAxis);
+
+    y.domain([d3.min(data, function (d) { return d.ser2 }), d3.max(data, function (d) { return d.ser2 })]);
+    svg.selectAll(".myYaxis")
+        .transition()
+        .duration(3000)
+        .call(yAxis);
+
+    const u = svg.selectAll(".lineTest")
+        .data([data], function (d) { return d.ser1 });
+
+    u
+        .join("path")
+        .attr("class", "lineTest")
+        .transition()
+        .duration(3000)
+        .attr("d", d3.line()
+            .x(function (d) { return x(d.ser1); })
+            .y(function (d) { return y(d.ser2); }))
+        .attr("fill", "none")
+        .attr("stroke", "#b2001d")
+        .attr("stroke-width", 3.5)
+}
+
